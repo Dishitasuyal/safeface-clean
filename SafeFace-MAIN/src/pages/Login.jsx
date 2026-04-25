640da157bb91efdf14454774842897baf4e488d8
Dishitasuyal <dishita.suyal@gmail.com>
2026-04-25 15:11:37 +0530

    Update Login.jsx

--- SafeFace-MAIN/src/pages/Login.jsx
+++ SafeFace-MAIN/src/pages/Login.jsx
@@ -11,7 +11,7 @@ function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
 
-    const response = await fetch(""https://safeface-clean-bl8z.onrender.com/login"", {
+    const response = await fetch("https://safeface-clean-bl8z.onrender.com/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
